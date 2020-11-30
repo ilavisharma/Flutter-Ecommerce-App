22234d7470880a07bbed04677c6e60ac945c481c
Lavi Sharma <lavisharma796@gmail.com>
2020-11-30 18:12:46 +0530

    custom appbar finally done

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -5,30 +5,74 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget leading;
   final Widget trailing;
 
-  CustomAppBar(
-      {@required this.title, @required this.leading, @required this.trailing});
+  CustomAppBar({
+    @required this.title,
+    @required this.leading,
+    @required this.trailing,
+  });
 
   @override
-  Size get preferredSize => Size.fromHeight(75);
+  Size get preferredSize => Size.fromHeight(150);
 
   Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.white),
+      borderRadius: BorderRadius.all(Radius.circular(40)),
+    );
     return Container(
-      height: MediaQuery.of(context).padding.top + 75,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color(0xFF845FA1), Color(0xFF34283E)],
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-        ),
-      ),
-      padding: EdgeInsets.only(
-          left: 15, right: 15, top: MediaQuery.of(context).padding.top),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
         children: [
-          leading,
-          title,
-          trailing,
+          Align(
+            heightFactor: 0.8,
+            alignment: Alignment.topCenter,
+            child: Container(
+              height: 130,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFF845FA1), Color(0xFF34283E)],
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  leading,
+                  title,
+                  trailing,
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 23),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 10,
+                  blurRadius: 30,
+                ),
+              ],
+            ),
+            child: TextField(
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "What are you looking for?",
+                  prefixIcon: Icon(Icons.search),
+                  border: border,
+                  enabledBorder: border,
+                  focusedBorder: border),
+            ),
+          )
         ],
       ),
     );
