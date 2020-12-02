0ad5400e820d3a766b8db491e77807e78ab81c98
Lavi Sharma <lavisharma796@gmail.com>
2020-12-02 19:55:32 +0530

    add the bottom modal sheet of categories on the catalogue screen

--- lib/screens/home/catalogue_screen.dart
+++ lib/screens/home/catalogue_screen.dart
@@ -4,32 +4,90 @@ import 'package:flutter/material.dart';
 class CatalogueScreen extends StatelessWidget {
   static const routeName = '/catalogue';
 
-  List<Widget> _buildCatalogue() {
-    return Catalogue.map((item) {
-      return Card(
-        shadowColor: Colors.black,
-        elevation: 2.5,
-        margin: EdgeInsets.symmetric(vertical: 8),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(18),
-              child: Text(
-                item['title'],
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
+  static const _categories = [
+    'Clothing',
+    'Shoes',
+    'Jewelry',
+    'Watches',
+    'Handbags',
+    'Accessories',
+    'Men\'s Fashion',
+    'Girls\' Fashion',
+    'Boys\' Fashion'
+  ];
+
+  void _handleItemClick(BuildContext context, String title) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            color: Color(0xFF737373),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).canvasColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  topRight: Radius.circular(24),
+                ),
+              ),
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(18),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 19),
+                      ),
+                    ),
+                    ..._categories
+                        .map(
+                          (cat) => Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8),
+                            child: Text(cat, style: TextStyle(fontSize: 15)),
+                          ),
+                        )
+                        .toList(),
+                  ],
                 ),
               ),
             ),
-            Image.asset(
-              item['image'],
-              height: 95,
-              width: 95,
-              fit: BoxFit.cover,
-            )
-          ],
+          );
+        });
+  }
+
+  List<Widget> _buildCatalogue(BuildContext context) {
+    return Catalogue.map((item) {
+      return InkWell(
+        onTap: () => _handleItemClick(context, item['title']),
+        child: Card(
+          shadowColor: Colors.black,
+          elevation: 2.5,
+          margin: EdgeInsets.symmetric(vertical: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(18),
+                child: Text(
+                  item['title'],
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Image.asset(
+                item['image'],
+                height: 95,
+                width: 95,
+                fit: BoxFit.cover,
+              )
+            ],
+          ),
         ),
       );
     }).toList();
@@ -46,7 +104,7 @@ class CatalogueScreen extends StatelessWidget {
           SizedBox(
             height: 145,
           ),
-          ..._buildCatalogue(),
+          ..._buildCatalogue(context),
         ],
       ),
     );
